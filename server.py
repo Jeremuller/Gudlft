1b5401ba117e4182cdde2d2e2d79dc10ba45bc27
Jeremy Muller <jeremymuller92@gmail.com>
2025-06-22 17:05:19 +0200

    Adding error handling:
    - Try/ except block with KeyError and unknown email handling
    
    - Using next to prevent IndexError

--- server.py
+++ server.py
@@ -28,8 +28,19 @@ def index():
 
 @app.route("/showSummary", methods=["POST"])
 def show_summary():
-    club = [club for club in clubs if club["email"] == request.form["email"]][0]
-    return render_template("welcome.html", club=club, competitions=competitions)
+    try:
+        email = request.form["email"]
+        club = next((club for club in clubs if club["email"] == email), None)
+
+        if club is None:
+            flash("The email you entered isn't found, please try again.")
+            return render_template("index.html")
+
+        return render_template("welcome.html", club=club, competitions=competitions)
+
+    except KeyError:
+        flash("Form isn't complete, please enter an email address.")
+        return render_template("index.html")
 
 
 @app.route("/book/<competition>/<club>")
