1e5e880a89b212c16c878132c7eae0ef1444efdc
Jeremy Muller <jeremymuller92@gmail.com>
2025-06-22 17:51:09 +0200

    Adding unittest for showSummary with valid email

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,9 +1,19 @@
 import pytest
 
-from ..server import clubs, app
+from server import clubs, app
+
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
+
+def test_show_summary_with_valid_email(client):
+    response = client.post("/showSummary", data={"email": "john@simplylift.co"})
+    assert response.status_code == 200
+    assert b"<h2>Welcome, john@simplylift.co" in response.data
+
+
+
