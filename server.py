83af4f9c8a2b391d49a8c3b5f2816fc19707bf97
Jeremy Muller <jeremymuller92@gmail.com>
2025-06-27 15:26:14 +0200

    adding line to update club poit after booking competitions places, unit test passed

--- server.py
+++ server.py
@@ -76,6 +76,7 @@ def purchase_places():
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
     places_required = int(request.form["places"])
     competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - places_required
+    club["points"] = int(club["points"]) - int(request.form["places"])
     flash("Great-booking complete!")
     return render_template("welcome.html", club=club, competitions=competitions)
 
