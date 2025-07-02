fb759e25d456ad43a25770f3bad072348048863a
Jeremy Muller <jeremymuller92@gmail.com>
2025-07-02 17:56:12 +0200

    Adding our new data fixture, and changes the path of the test_display_all_club_names_and_points(client)

--- tests/test_points_display_board.py
+++ tests/test_points_display_board.py
@@ -1,6 +1,6 @@
 import pytest
 
-from server import clubs, app, competitions
+from server import clubs, app, competitions, booked_places
 
 
 @pytest.fixture
@@ -14,11 +14,41 @@ def client():
         yield client
 
 
+@pytest.fixture
+def setup_data():
+    """Fixture to set up initial data for tests."""
+
+    # Copy from original datas
+    original_clubs = clubs.copy()
+    original_competitions = competitions.copy()
+    original_booked_places = booked_places.copy()
+
+    # Add test datas for club and competition
+    clubs.append({"name": "Test Club", "email": "test@club.com", "points": 10})
+    competitions.append(
+        {
+            "name": "Test Competition",
+            "date": "2026-12-01 10:00:00",
+            "numberOfPlaces": 10,
+        }
+    )
+
+    yield
+
+    # Restore original datas
+    clubs.clear()
+    clubs.extend(original_clubs)
+    competitions.clear()
+    competitions.extend(original_competitions)
+    booked_places.clear()
+    booked_places.update(original_booked_places)
+
+
 def test_display_all_club_names_and_points(client):
     """
     Test that all clubs and their points are correctly displayed on the welcome page.
     """
-    response = client.post("/showSummary", data={"email": "admin@irontemple.com"})
+    response = client.post("/", data={"email": "admin@irontemple.com"})
     assert response.status_code == 200
 
     for club in clubs:
