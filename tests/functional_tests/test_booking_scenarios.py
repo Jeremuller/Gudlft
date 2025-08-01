1b144eec79ad2a44e83c4292e742b60155a80bd6
Jeremy Muller <jeremymuller92@gmail.com>
2025-08-01 17:51:33 +0200

    Adding fifth scenario in test_booking_scenarios.py functional tests 8/10
    test_booking_scenarios.py is now complete.
    
    A few tests are failing sometimes, but I think it's more an issue about the amount of successive operations creating issue sometimes.

--- tests/functional_tests/test_booking_scenarios.py
+++ tests/functional_tests/test_booking_scenarios.py
@@ -59,11 +59,14 @@ def test_successful_booking(driver, create_app):
     )
     submit_button.click()
 
-    # Book places
-    book_link = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Book Places"))
+    # Find all "Book Places" links and select the third one (corresponding to test competition)
+    book_links = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.LINK_TEXT, "Book Places"))
     )
-    book_link.click()
+    if len(book_links) >= 3:
+        book_links[2].click()  # Select the third link (index 2)
+    else:
+        raise Exception("Not enough 'Book Places' links found.")
 
     places_field = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='places']"))
@@ -105,11 +108,14 @@ def test_invalid_data_booking(driver, create_app):
     )
     submit_button.click()
 
-    # Attempt to book places with invalid data
-    book_link = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Book Places"))
+    # Find all "Book Places" links and select the third one (corresponding to test competition)
+    book_links = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.LINK_TEXT, "Book Places"))
     )
-    book_link.click()
+    if len(book_links) >= 3:
+        book_links[2].click()  # Select the third link (index 2)
+    else:
+        raise Exception("Not enough 'Book Places' links found.")
 
     places_field = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='places']"))
@@ -151,17 +157,20 @@ def test_insufficient_points_booking(driver, create_app):
     )
     submit_button.click()
 
-    # Attempt to book places with insufficient points
-    book_link = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Book Places"))
+    # Find all "Book Places" links and select the third one (corresponding to test competition)
+    book_links = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.LINK_TEXT, "Book Places"))
     )
-    book_link.click()
+    if len(book_links) >= 3:
+        book_links[2].click()  # Select the third link (index 2)
+    else:
+        raise Exception("Not enough 'Book Places' links found.")
 
     places_field = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='places']"))
     )
     places_field.clear()
-    places_field.send_keys("999")  # Assuming the user doesn't have this many points
+    places_field.send_keys("40")  # Assuming the user doesn't have this many points
 
     book_submit_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
@@ -197,11 +206,14 @@ def test_limit_places_booking(driver, create_app):
     )
     submit_button.click()
 
-    # Attempt to book places with insufficient competition places
-    book_link = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Book Places"))
+    # Find all "Book Places" links and select the third one (corresponding to test competition)
+    book_links = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.LINK_TEXT, "Book Places"))
     )
-    book_link.click()
+    if len(book_links) >= 3:
+        book_links[2].click()  # Select the third link (index 2)
+    else:
+        raise Exception("Not enough 'Book Places' links found.")
 
     places_field = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='places']"))
@@ -219,4 +231,62 @@ def test_limit_places_booking(driver, create_app):
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".flash-messages"),
                                          "A club cannot book more than 12 places in total for a competition.")
     )
+    assert flash_message
+
+def test_insufficient_places_booking(driver, create_app):
+    """
+    Test booking with insufficient competition places.
+    """
+
+    def run_app():
+        create_app.run(port=5001)
+
+    app_thread = Thread(target=run_app)
+    app_thread.daemon = True
+    app_thread.start()
+
+    # Log in
+    driver.get("http://localhost:5001/")
+    email_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']"))
+    )
+    email_field.clear()
+    email_field.send_keys("functional_test@club.co")
+
+    submit_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+    )
+    submit_button.click()
+
+    # Find all "Book Places" links and select the third one
+    book_links = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.LINK_TEXT, "Book Places"))
+    )
+    if len(book_links) >= 3:
+        book_links[2].click()  # Select the third link (index 2)
+    else:
+        raise Exception("Not enough 'Book Places' links found.")
+
+    places_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='places']"))
+    )
+    places_field.clear()
+    places_field.send_keys("21")  # Assuming there aren't this many places available
+
+    book_submit_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+    )
+    book_submit_button.click()
+
+    # Wait for the flash message to be present in the DOM
+    try:
+        flash_message = WebDriverWait(driver, 20).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".flash-messages"),
+                                             "Not enough places available in the competition.")
+        )
+        print("Flash message found:", flash_message)
+    except Exception as e:
+        print("Flash message not found. Page source:", driver.page_source)
+        raise e
+
     assert flash_message
